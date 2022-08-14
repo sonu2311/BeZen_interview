ff7a17929f3b8d952060adff7af783a68e59347c
Sonu Saini <sonu2311saini@gmail.com>
2022-08-15 01:24:15 +0530

    delete example file

--- src/common.jsx
+++ src/common.jsx
@@ -1,6 +1,3 @@
-import Example1 from './example1';
-import Example2 from './example2';
-import Example3 from './example3';
 import {Routes, Route, HashRouter} from "react-router-dom"
 import React from 'react';
 import { Home } from './home';
@@ -11,9 +8,6 @@ function MainFunc() {
       <Routes>
         <Route >
           <Route path="/" element={<Home/>} />
-          <Route path="/example1" element={<Example1 />} />
-          <Route path="/example2" element={<Example2 />} />
-          <Route path="/example3" element={<Example3 />} />
           <Route path="/home" element={<Home/>} />
           <Route path="*" element={<h1>Invalid</h1>} />
         </Route>
